e9e35ac20c18cae2ad1ac62b2ea7af9886a0baf4
Jhosehp <jhosehp.rendon@gmail.com>
2018-05-11 11:59:18 -0500

    Automated cache clearing with middleware

--- services/cache.js
+++ services/cache.js
@@ -4,17 +4,26 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
-
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function(options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+    return this;
+}
+
 mongoose.Query.prototype.exec = async function() {
+
+    if(!this.useCache) {
+        return exec.apply(this, arguments)
+    }
 
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     }));
 
-    const cacheValue = await client.get(key);
+    const cacheValue = await client.hget(this.hashKey, key);
 
     if(cacheValue) {
         const doc = JSON.parse(cacheValue);
@@ -24,7 +33,13 @@ mongoose.Query.prototype.exec = async function() {
 
     const result = await exec.apply(this, arguments);
 
-    client.set(key, JSON.stringify(result));
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
     return result
+};
+
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    }
 }
