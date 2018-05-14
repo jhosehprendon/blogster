54e5df627a5b9a2bb4a9c03aee009059abe9bf35
Jhosehp <jhosehp.rendon@gmail.com>
2018-05-14 17:36:54 -0500

    Add AWS S3 keys and view the signed URL

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,8 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://jhosehp:rendon@ds017726.mlab.com:17726/quora_dev',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAJZDAUY6VZCTJ7MEA',
+  secretAccessKey: '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'
 };
