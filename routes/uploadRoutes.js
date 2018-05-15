9c8cbd47b89435d8b55abf1693acba2a8e60d757
Jhosehp <jhosehp.rendon@gmail.com>
2018-05-14 21:05:58 -0500

    Fix CORS

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -5,7 +5,9 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const s3 = new AWS.S3({
     accessKeyId: keys.accessKeyId,
-    secretAccessKey: keys.secretAccessKey
+    secretAccessKey: keys.secretAccessKey,
+    signatureVersion: 'v4',
+    region: 'us-east-2'
 });
 
 module.exports = app => {
@@ -14,7 +16,7 @@ module.exports = app => {
         const key = `${req.user.id}/${uuid()}.jpeg`;
         s3.getSignedUrl('putObject', {
             Bucket: 'my-blog-bucket-jr',
-            ContentType: 'jpeg',
+            ContentType: 'image/jpeg',
             Key: key
         }, (err, url) => res.send({key, url}));
     });
